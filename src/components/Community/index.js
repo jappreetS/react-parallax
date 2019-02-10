044324e1ea16f1617a74dd10f36a058d55c70963
Jappreet Singh <jappreet.singh@decurtis.com>
2019-02-11 02:35:46 +0530

    aos setup

--- src/components/Community/index.js
+++ src/components/Community/index.js
@@ -5,7 +5,12 @@ import './Community.scss';
 class Community extends Component {
   render() {
     return (
-      <div id="community-container" className="community-container"></div>
+      <div id="community-container" className="community-container">
+        <div data-aos="fade-up"
+          data-aos-anchor-placement="center-center"
+          className="community-food-section">
+        </div>
+      </div>
     );
   }
 }
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Redirect,
   Route,
   Switch,
 } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import Home from './../Home';
 
-const Main = () => (
-  <React.Fragment>
-    <Switch>
-      <Redirect exact from='/' to='/parallax' component={Home} />
-      <Route exact path='/parallax' component={Home} />
-    </Switch>
-  </React.Fragment>
-);
+class Main extends Component {
+  constructor() {
+    super();
+    AOS.init(({
+      duration: 1000,
+    }));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Switch>
+          <Redirect exact from='/' to='/parallax' component={Home} />
+          <Route exact path='/parallax' component={Home} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Main;
