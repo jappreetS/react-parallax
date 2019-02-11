e3eefce5641ab6761cc581a55ff627ee06b00f97
Jappreet Singh <jappreet.singh@outlook.com>
2019-02-12 01:31:55 +0530

    community section animation setup

--- src/components/Community/index.js
+++ src/components/Community/index.js
@@ -6,10 +6,26 @@ class Community extends Component {
   render() {
     return (
       <div id="community" className="community-container">
-        <div data-aos="fade-up"
-          data-aos-anchor-placement="center-center"
-          className="community-food-section">
-        </div>
+        <div className="community-description" />
+        <div data-aos="slide-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-duration="1000"
+          data-aos-easing="linear"
+          data-offset-top="1000"
+          className="community-food-section-1"
+        />
+        <div data-aos="slide-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-duration="1000"
+          data-aos-easing="linear"
+          className="community-food-section-2"
+        />
+        <div data-aos="slide-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-duration="1000"
+          data-aos-easing="linear"
+          className="community-food-section-3"
+        />
       </div>
     );
   }
