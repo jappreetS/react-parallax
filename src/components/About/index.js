66219f135d4aa986a3f6f2a611fc08ad26c0042e
Jappreet Singh <jappreet.singh@outlook.com>
2019-02-12 03:18:40 +0530

    added stamp image in about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import stamp from './../../assets/images/stamp.png';
 import './About.scss';
 
 class About extends Component {
   render() {
     return (
       <div id="about" className="about-container text-uppercase">
+        <img alt="stamp" src={stamp} className="stamp" />
         <div className="line-1">The Best Foodie</div>
         <div className="line-2">Experience</div>
         <div className="line-3">Now in London</div>
