1733cd560d93b6cde1e3635ebfdb9b162160eaea
Jappreet Singh <jappreet.singh@outlook.com>
2019-02-14 22:59:18 +0530

    Button Component updates

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -3,11 +3,16 @@ import React from 'react';
 import './Button.scss';
 
 const Button = ({
+  showBorder,
   text,
   filled
 }) =>
   (
-    <button className={filled ? 'primary' : 'secondary'}>{text}</button>
+    <button
+      className={filled ? `primary` : `secondary ${showBorder ? null : 'border'}`}
+    >
+      {text}
+    </button>
   )
 
 export default Button;
